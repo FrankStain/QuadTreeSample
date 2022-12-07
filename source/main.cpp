639457ea80ead0803c918b89cc549f77cfe4f824
Frank Stein <dr.frank.stain@gmail.com>
2022-12-07 22:30:04 +0300

    Move includes to main header.

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,4 @@
-#include <cstdint>
-#include <cmath>
-#include <array>
-#include <vector>
-#include <list>
-#include <queue>
-#include <iterator>
-#include <memory>
-#include <optional>
-#include <variant>
-#include <utility>
-#include <algorithm>
-#include <random>
+#include <main.h>
 
 
 struct Vector2f final
---
+++ source/main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include <cstdint>
+#include <cmath>
+#include <array>
+#include <vector>
+#include <list>
+#include <queue>
+#include <iterator>
+#include <memory>
+#include <optional>
+#include <variant>
+#include <utility>
+#include <algorithm>
+#include <random>
