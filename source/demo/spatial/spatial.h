9dc91a9e0e5a154eee40a1d7ad07cab165f1f351
Frank Stein <dr.frank.stain@gmail.com>
2022-12-07 23:10:49 +0300

    Main header for spatial subsystem.

---
+++ source/demo/spatial/spatial.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+// Most fundamental dependencies.
+#include <demo/math/math.h>
+
+
+// Namespace definition.
+namespace Demo
+{
+// Spatial subsystem.
+inline namespace Spatial
+{
+// Internal stuff for spatial interface.
+namespace Internal
+{
+
+}
+}
+}
+
+
+// Most fundamental declarations.
+
+// Internal definitions.
+
+// Public definitions.
+
+// Deferred inline definitions.
