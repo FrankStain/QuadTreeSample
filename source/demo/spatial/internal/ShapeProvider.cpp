6ddbba493d4f85142362867854b26df7e36382e8
Frank Stein <dr.frank.stain@gmail.com>
2022-12-09 00:05:18 +0300

    Shape provider refactored to look more gentle.

--- source/demo/spatial/internal/ShapeProvider.cpp
+++ source/demo/spatial/internal/ShapeProvider.cpp
@@ -7,9 +7,34 @@ inline namespace Spatial
 {
 namespace Internal
 {
+namespace
+{
+	const ShapeProvider::Handle ToHandle( const size_t bucket_index, const size_t slot_index )
+	{
+		return ShapeProvider::Handle{ bucket_index * ShapeProvider::BUCKET_LENGTH + slot_index };
+	}
+
+	std::pair<size_t, size_t> FromHandle( const ShapeProvider::Handle handle )
+	{
+		return { size_t( handle ) / ShapeProvider::BUCKET_LENGTH, size_t( handle ) % ShapeProvider::BUCKET_LENGTH };
+	}
+
+	const bool HasFreeSlot( const ShapeProvider::Bucket& bucket )
+	{
+		return std::any_of( bucket.begin(), bucket.end(), []( const std::optional<Shape>& slot ) { return !slot.has_value(); } );
+	}
+
+	std::optional<Shape>& GetSlot( const ShapeProvider::BucketStorage& storage, const ShapeProvider::Handle handle )
+	{
+		auto [ bucket_index, slot_index ] = FromHandle( handle );
+		return storage.at( bucket_index )->at( slot_index );
+	}
+}
+
+
 	std::pair<Shape*, ShapeProvider::Handle> ShapeProvider::Create( QuadTree& host, const BoundingRect& bounds )
 	{
-		auto suitable_bucket = std::find_if( m_slots.begin(), m_slots.end(), []( const std::unique_ptr<Bucket>& bucket ) { return HasSlot( *bucket ); } );
+		auto suitable_bucket = std::find_if( m_slots.begin(), m_slots.end(), []( const std::unique_ptr<Bucket>& bucket ) { return HasFreeSlot( *bucket ); } );
 		if( suitable_bucket == m_slots.end() )
 		{
 			m_slots.emplace_back( std::make_unique<Bucket>() );
@@ -20,31 +45,18 @@ namespace Internal
 		auto suitable_slot = std::find_if( bucket.begin(), bucket.end(), []( const std::optional<Shape>& slot ) { return !slot.has_value(); } );
 
 		Shape& shape = suitable_slot->emplace( host, bounds );
-		return { &shape, Handle{ std::distance( m_slots.begin(), suitable_bucket ) * BUCKET_LENGTH + std::distance( bucket.begin(), suitable_slot ) } };
+		return { &shape, ToHandle( std::distance( m_slots.begin(), suitable_bucket ), std::distance( bucket.begin(), suitable_slot ) ) };
 	}
 
 	void ShapeProvider::Destroy( const Handle handle )
 	{
-		GetSlot( handle ).reset();
+		GetSlot( m_slots, handle ).reset();
 	}
 
 	Shape* ShapeProvider::Query( const Handle handle ) const
 	{
-		auto& slot = GetSlot( handle );
+		auto& slot = GetSlot( m_slots, handle );
 		return ( slot.has_value() )? &slot.value() : nullptr;
-	}
-
-	const bool ShapeProvider::HasSlot( const Bucket& bucket )
-	{
-		return std::any_of( bucket.begin(), bucket.end(), []( const std::optional<Shape>& slot ) { return !slot.has_value(); } );
-	}
-
-	std::optional<Shape>& ShapeProvider::GetSlot( const Handle handle ) const
-	{
-		const size_t bucket_index	= size_t( handle ) / BUCKET_LENGTH;
-		const size_t slot_index		= size_t( handle ) % BUCKET_LENGTH;
-
-		return m_slots.at( bucket_index )->at( slot_index );
 	}
 }
 }
--- source/demo/spatial/internal/ShapeProvider.h
+++ source/demo/spatial/internal/ShapeProvider.h
@@ -10,6 +10,12 @@ namespace Internal
 	class ShapeProvider final
 	{
 	public:
+		static constexpr size_t BUCKET_LENGTH = 8;
+
+	public:
+		using Bucket = std::array<std::optional<Shape>, BUCKET_LENGTH>;
+		using BucketStorage = std::vector<std::unique_ptr<Bucket>>;
+
 		enum class Handle : size_t;
 
 	public:
@@ -19,19 +25,7 @@ namespace Internal
 		Shape* Query( const Handle handle ) const;
 
 	private:
-		static constexpr size_t BUCKET_LENGTH = 8;
-
-	private:
-		using Bucket = std::array<std::optional<Shape>, BUCKET_LENGTH>;
-
-	private:
-		static const bool HasSlot( const Bucket& bucket );
-
-	private:
-		std::optional<Shape>& GetSlot( const Handle handle ) const;
-
-	private:
-		std::vector<std::unique_ptr<Bucket>> m_slots;
+		BucketStorage m_slots;
 	};
 }
 }
