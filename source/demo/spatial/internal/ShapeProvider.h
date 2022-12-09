06cc2a67a4ba86abe7e31e37d77cd4c2bd9a8d5c
Frank Stein <dr.frank.stain@gmail.com>
2022-12-09 08:47:05 +0300

    Minor fix.

--- source/demo/spatial/internal/ShapeProvider.h
+++ source/demo/spatial/internal/ShapeProvider.h
@@ -13,10 +13,10 @@ namespace Internal
 		static constexpr size_t BUCKET_LENGTH = 8;
 
 	public:
+		enum class Handle : size_t;
+
 		using Bucket = std::array<std::optional<Shape>, BUCKET_LENGTH>;
 		using BucketStorage = std::vector<std::unique_ptr<Bucket>>;
-
-		enum class Handle : size_t;
 
 	public:
 		std::pair<Shape*, Handle> Create( QuadTree& host, const BoundingRect& bounds );
