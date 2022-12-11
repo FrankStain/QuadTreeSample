df6e07bc7fd940decbe6f0d982ff4012f092b4fc
Frank Stein <dr.frank.stain@gmail.com>
2022-12-11 15:45:20 +0300

    Docs for shape point. Position set function moved to source file.

---
+++ source/demo/spatial/internal/Point.cpp
@@ -0,0 +1,15 @@
+#include <demo/spatial/spatial.h>
+
+namespace Demo
+{
+inline namespace Spatial
+{
+namespace Internal
+{
+	void Point::SetPosition( const Vector2f& position )
+	{
+		m_position = position;
+	}
+}
+}
+}
