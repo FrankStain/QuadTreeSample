22f40735f1ffe9f9fb76ea94f5cac6b03b3915c4
Frank Stein <dr.frank.stain@gmail.com>
2022-12-11 12:56:12 +0300

    Docs for some forwards.

--- source/demo/spatial/internal/forwards.h
+++ source/demo/spatial/internal/forwards.h
@@ -6,8 +6,13 @@ inline namespace Spatial
 {
 namespace Internal
 {
+	// Allow the aliases to use points.
 	class Point;
+
+	// Allow the points to reference the shape.
 	class Shape;
+
+	// Allow the aliases to use quads.
 	struct Quad;
 }
 }
