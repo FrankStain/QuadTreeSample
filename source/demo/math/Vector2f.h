b649162a7fdb61d1de63d07901b63d27ca047aa9
Frank Stein <dr.frank.stain@gmail.com>
2022-12-11 10:48:49 +0300

    Docs for 2d vector.

--- source/demo/math/Vector2f.h
+++ source/demo/math/Vector2f.h
@@ -5,15 +5,24 @@ namespace Demo
 {
 inline namespace Math
 {
+	/**
+		@brief	2D vector of float elements.
+
+		Vector represents the `(x, y)` point or direction in 2D space.
+		It also provide some subset of functions that necessary for this project.
+		Vector instances are swappable.
+	*/
 	struct Vector2f final
 	{
+		// Default value for fields.
 		static constexpr float NAN_VALUE = NAN;
 
 
-		float	x = NAN_VALUE;
-		float	y = NAN_VALUE;
+		float x = NAN_VALUE;	// X axis.
+		float y = NAN_VALUE;	// Y axis.
 
 
+		// `swap` function support.
 		inline friend void swap( Vector2f& left, Vector2f& right ) noexcept	{ left.Swap( right ); };
 
 
@@ -29,16 +38,27 @@ inline namespace Math
 		inline Vector2f& operator = ( Vector2f&& other ) noexcept	{ Vector2f{ std::move( other ) }.Swap( *this ); return *this; };
 
 
+		// Performs the value swapping.
 		inline void Swap( Vector2f& other ) noexcept;
 
 
+		// Makes this vector to be minimal against other one. For each axis of this vector performs `min(this, other)`.
 		inline Vector2f& Minimize( const Vector2f& other );
+
+		// Produces the minimal vector between this one and other. For each axis of this vector performs `min(this, other)`.
 		inline Vector2f Minimized( const Vector2f& other ) const;
+
+		// Makes this vector to be maximal against other one. For each axis of this vector performs `max(this, other)`.
 		inline Vector2f& Maximize( const Vector2f& other );
+
+		// Produces the maximal vector between this one and other. For each axis of this vector performs `max(this, other)`.
 		inline Vector2f Maximized( const Vector2f& other ) const;
 
 
+		// Calculates the quadratic (^2) length of vector.
 		inline const float GetSquareLength() const;
+
+		// Calculates the length of vector.
 		inline const float GetLength() const;
 	};
 }
