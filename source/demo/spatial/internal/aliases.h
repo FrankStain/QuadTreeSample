7b2e1c433754abd2ca79fc0d4732a2c37a8cfd08
Frank Stein <dr.frank.stain@gmail.com>
2022-12-11 13:04:18 +0300

    Docs for aliases.

--- source/demo/spatial/internal/aliases.h
+++ source/demo/spatial/internal/aliases.h
@@ -7,9 +7,14 @@ inline namespace Spatial
 {
 namespace Internal
 {
-	using Points	= std::vector<const Point*>;
-	using Quads		= std::array<std::shared_ptr<Quad>, Demo::BoundingRect::CORNERS_COUNT>;
-	using QuadState	= std::variant<Points, Quads>;
+	// The state of quad that represents the leaf of quad tree. Only leafs store the points.
+	using Points = std::vector<const Point*>;
+
+	// The state of quad that represent the root of subtree.
+	using Quads = std::array<std::shared_ptr<Quad>, Demo::BoundingRect::CORNERS_COUNT>;
+
+	// The state of quad.
+	using QuadState = std::variant<Points, Quads>;
 }
 }
 }
