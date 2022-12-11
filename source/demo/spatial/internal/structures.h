25e3365f4a2cedab63077c931cc860aaf834a05c
Frank Stein <dr.frank.stain@gmail.com>
2022-12-11 15:26:41 +0300

    Docs for internal structures.

--- source/demo/spatial/internal/structures.h
+++ source/demo/spatial/internal/structures.h
@@ -7,12 +7,24 @@ inline namespace Spatial
 {
 namespace Internal
 {
+	/**
+		@brief	Quad tree quadrant.
+
+		Quads are the nodes in quad tree. Single quadrant can be in one of two states:
+
+		1. The list quad, that can index the points. Such quad can't carry subtree of quads.
+		2. The root of subtree, that carry it's own subtree of quads. Such quad can't index the points.
+
+		Each quad is placed at some level of quad tree and describes it's own bounding rect.
+		If quad represents leaf, it's bound consists of each indexed point.
+		If quad represents subtree, stored quads represent the quarters of quad bounds.
+	*/
 	struct Quad final
 	{
-		BoundingRect	bounds;
-		Vector2f		center;
-		size_t			level;
-		QuadState		state;
+		BoundingRect	bounds;	// Bounding rect of quadrant.
+		Vector2f		center;	// Center of quadrant bounds.
+		size_t			level;	// Level of quadrant in quad tree.
+		QuadState		state;	// State of quadrant.
 	};
 }
 }
