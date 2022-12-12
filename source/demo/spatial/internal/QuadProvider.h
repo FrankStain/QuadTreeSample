9ef54686ee6599db0cc977098ba2009eb772957b
Frank Stein <dr.frank.stain@gmail.com>
2022-12-12 23:06:27 +0300

    Docs for quad provider.

--- source/demo/spatial/internal/QuadProvider.h
+++ source/demo/spatial/internal/QuadProvider.h
@@ -7,16 +7,27 @@ inline namespace Spatial
 {
 namespace Internal
 {
+	/**
+		@brief	Provider of quad instances.
+
+		This provider is a mock for true quad memory pool.
+		Currently it only gives the valid interface to create the quads.
+	*/
 	class QuadProvider final
 	{
+	// Public interface.
 	public:
+		// Create new quad. The instance returned will be destroyed once there no shared pointers reference it.
 		std::shared_ptr<Quad> Create( const BoundingRect& bounds, const size_t level );
 
+	// Private interface.
 	private:
+		// Perform the quad destruction by given iterator.
 		void Destroy( std::list<Quad>::iterator slot );
 
+	// Private state.
 	private:
-		std::list<Quad>	m_slots;
+		std::list<Quad>	m_slots; // Storage for quads.
 	};
 }
 }
