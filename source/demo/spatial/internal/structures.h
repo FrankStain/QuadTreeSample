d1fa3764af81ffd7c64a818ac0c77c3c58a590f6
Frank Stein <dr.frank.stain@gmail.com>
2022-12-27 23:18:35 +0300

    Docs changed.

--- source/demo/spatial/internal/structures.h
+++ source/demo/spatial/internal/structures.h
@@ -10,10 +10,10 @@ namespace Internal
 	/**
 		@brief	Quad tree quadrant.
 
-		Quads are the nodes in quad tree. Single quadrant can be in one of two states:
-
-		1. The list quad, that can index the points. Such quad can't carry subtree of quads.
-		2. The root of subtree, that carry it's own subtree of quads. Such quad can't index the points.
+		Quads are the nodes in quad tree. Each quad can simultaneously store the shapes and quarter quads.
+		After the creation the quad represents the leaf. Leafs store only shapes. But any leaf may be converted to subtree.
+		Once the quad becomes subtree, it still store the shapes. But subtree stores only shapes that intersects with more than one quarter of quad.
+		So that, the shapes are indexed by the most local quad, where the shape is completely inside.
 
 		Each quad is placed at some level of quad tree and describes it's own bounding rect.
 		If quad represents leaf, it's bound consists of each indexed point.
